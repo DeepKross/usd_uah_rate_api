fb1f5825bdde8c6903be23cbe0c4390fad017990
Mykhailo Tanhcuk <mykhailo.tanchuk@eliftech.com>
2024-05-16 20:17:38 +0300

    Config and Logger
    feat: added request and actions loggers ( winston and morgan). Configured them as needed.
    feat: make config file for all env with validation and type check

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, json, urlencoded } from 'express';
+import { Server } from 'http';
 import favicon from 'serve-favicon';
 
+import logger from './config/logger';
 import router from './routes/v1/index';
 import prisma from './prismaClient';
 
@@ -32,10 +34,40 @@ app.use(json());
 //all routes are prefixed with /v1
 app.use(router);
 
+let server: Server;
+
 //connect to the database and start the server
 void prisma.$connect().then(() => {
   console.info('Connected to SQL Database');
   app.listen(port, () => {
     console.info(`Listening to port http://localhost:${port}`);
   });
+});
+
+//exception handling
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error: unknown) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+
+  if (server) {
+    server.close();
+  }
 });
--- src/services/healthcheck.services.ts
+++ src/services/healthcheck.services.ts
@@ -1,3 +1,7 @@
+import logger from '../config/logger';
+
 export const getHealthcheckMsg = () => {
+  logger.info('Server is up and running!');
+
   return 'Server is up and running!';
 };
